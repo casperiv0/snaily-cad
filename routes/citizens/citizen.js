e34b6902aa62a3e3747794c1238f527c09bf66e8
Casper Iversen <Casper@News-MacBook-Air.local>
2020-02-23 15:31:16 +0100

    edit fix

--- routes/citizens/citizen.js
+++ routes/citizens/citizen.js
@@ -93,7 +93,7 @@ module.exports = {
     },
     editCitizen: (req, res) => {
         let id = req.params.id
-        let first_name = req.session.username2;
+        let first_name = req.body.full_name;
         // let last_name = req.body.last_name;
         let last_name = "Unknown";
         // let full_name = first_name + " " + last_name;
