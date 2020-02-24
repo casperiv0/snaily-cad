8a8e340944c499d3fe54f742b18e3ecc2ad2f494
Casper Iversen <Casper@Macbookerini.local>
2020-02-24 16:07:51 +0100

    fixing car reg 2

--- routes/values/cars.js
+++ routes/values/cars.js
@@ -112,8 +112,8 @@ module.exports = {
         let q1 = "SELECT plate FROM `registered_cars` WHERE plate = '"+ plate+ "'"
 
         db.query(q1, (err, result) => {
-            console.log(result[0])
-            if (plate == result[0]) {
+            console.log(result)
+            if (`${plate}` === `${result}`) {
                 res.send('Plate does not exist')
             } else {
                 res.send('plate does exist')
